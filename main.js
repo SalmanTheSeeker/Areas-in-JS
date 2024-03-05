a03599c5c4b9c6f028832c9144f6d7a695e566c3
SalluXio <me@sallu.io>
2024-03-05 12:15:55 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// Calculating Area of Rectangle
+
+var lenth = 6
+var width = 9
+var arreaRectangle = lenth*width
+
+messageRectangle = `
+Calculating Area of Rectangle ▭
+lenth = 6
+width = 9
+arreaRectangle = ${arreaRectangle}
+`
+console.log(messageRectangle);
+
+
+
+// Calculating Area of Triangle
+
+var base = 5
+var height = 4
+var areaTriangle = (base*height)/2
+
+messageTriangle = `
+Calculating Area of Triangle ▲
+base = 5
+height = 4
+areaTriangle = ${areaTriangle}
+`
+console.log(messageTriangle);
+
+
+
+
+
+// Calculating Area of R
+
+
+var radius = 6
+var pi = 3.14
+var areaCircle = pi*(radius**2)
+
+messageCircle = `
+Calculating Area of Circle o
+radius = 6
+pi = 3.14
+areaCircl = ${areaCircle}
+`
+console.log(messageCircle);
